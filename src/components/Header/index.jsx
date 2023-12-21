29df1934f5b1668af640f3d1067898e277b41f76
Larissa Moreira <larimlisboa@gmail.com>
2023-12-21 12:17:08 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import { RiShutDownLine } from "react-icons/ri";
+import { Container, Profile, Logout } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/laralis.png" alt="Foto de perfil" />
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Larissa Lisboa</strong>
+        </div>
+      </Profile>
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
+}
