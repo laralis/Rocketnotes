a7866fe8938ab16f94215cdac25cf755d5754b15
Larissa Moreira <larimlisboa@gmail.com>
2024-01-17 14:12:03 -0300

    Add hooks and useStates

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
-import  GlobalStyles  from './styles/global';
+import GlobalStyles from "./styles/global";
 import theme from "./styles/theme";
+import { AuthProvider } from "./hooks/auth";
 import { Routes } from "./routes";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <Routes />
+      <GlobalStyles />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
